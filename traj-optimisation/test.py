5e4c3418e5aaca5b1d5ede97d264d1c43cc0f936
arthur <arthur.haffemayer@yahoo.fr>
2024-02-16 10:27:44 +0100

    new formulation of the constraint

--- traj-optimisation/test.py
+++ traj-optimisation/test.py
@@ -1,109 +1,138 @@
 from os.path import dirname, join, abspath
-import time
 import numpy as np
 
 import pinocchio as pin
 import hppfcl
-
+import copy
 from wrapper_meshcat import MeshcatWrapper
 from wrapper_robot import RobotWrapper
-from ocp_panda_reaching import OCPPandaReaching
-from ocp_panda_reaching_obs_single_point import OCPPandaReachingColWithSingleCol
 
 from utils import BLUE, YELLOW_FULL
 
-### PARAMETERS
-# Number of nodes of the trajectory
-T = 200
-# Time step between each node
-dt = 0.001
-
-
-### LOADING THE ROBOT
-pinocchio_model_dir = join(dirname(dirname(dirname(str(abspath(__file__))))), "models")
-model_path = join(pinocchio_model_dir, "franka_description/robots")
-mesh_dir = pinocchio_model_dir
-urdf_filename = "franka2.urdf"
-urdf_model_path = join(join(model_path, "panda"), urdf_filename)
-srdf_model_path = model_path + "/panda/demo.srdf"
-
-# Creating the robot
-robot_wrapper = RobotWrapper(
-    urdf_model_path=urdf_model_path, mesh_dir=mesh_dir, srdf_model_path=srdf_model_path
-)
-rmodel, cmodel, vmodel = robot_wrapper()
-rdata = rmodel.createData()
-cdata = cmodel.createData()
+## HELPERS 
 
 ### CREATING THE TARGET
 TARGET_POSE = pin.SE3(pin.utils.rotate("x", np.pi), np.array([0, 0, 1.55]))
-TARGET_POSE.translation = np.array([0, -0.4, 1.5])
-
 ### CREATING THE OBSTACLE
 OBSTACLE_RADIUS = 1.5e-1
 OBSTACLE_POSE = pin.SE3.Identity()
 OBSTACLE_POSE.translation = np.array([0.25, -0.425, 1.5])
-OBSTACLE = hppfcl.Sphere(OBSTACLE_RADIUS)
-OBSTACLE_GEOM_OBJECT = pin.GeometryObject(
-    "obstacle",
-    rmodel.getFrameId("universe"),
-    rmodel.frames[rmodel.getFrameId("universe")].parent,
-    OBSTACLE,
-    OBSTACLE_POSE,
+
+def panda_loader():
+    ### LOADING THE ROBOT
+    pinocchio_model_dir = join(dirname(dirname(str(abspath(__file__)))), "models")
+    model_path = join(pinocchio_model_dir, "franka_description/robots")
+    mesh_dir = pinocchio_model_dir
+    urdf_filename = "franka2.urdf"
+    urdf_model_path = join(join(model_path, "panda"), urdf_filename)
+    srdf_model_path = model_path + "/panda/demo.srdf"
+
+    # Creating the robot
+    robot_wrapper = RobotWrapper(
+        urdf_model_path=urdf_model_path, mesh_dir=mesh_dir, srdf_model_path=srdf_model_path
+    )
+    rmodel, cmodel, vmodel = robot_wrapper()
+    
+    OBSTACLE = hppfcl.Sphere(OBSTACLE_RADIUS)
+    OBSTACLE_GEOM_OBJECT = pin.GeometryObject(
+        "obstacle",
+        rmodel.getFrameId("universe"),
+        rmodel.frames[rmodel.getFrameId("universe")].parent,
+        OBSTACLE,
+        OBSTACLE_POSE,
+    )
+    OBSTACLE_GEOM_OBJECT.meshColor = BLUE
+    IG_OBSTACLE = cmodel.addGeometryObject(OBSTACLE_GEOM_OBJECT)
+    ## ADDING THE COLLISION PAIR BETWEEN A LINK OF THE ROBOT & THE OBSTACLE
+    cmodel.geometryObjects[cmodel.getGeometryId("panda2_link7_sc_2")].meshColor = YELLOW_FULL
+    cmodel.addCollisionPair(
+        pin.CollisionPair(cmodel.getGeometryId("panda2_link7_sc_2"), IG_OBSTACLE)
+    )
+    return rmodel, cmodel, vmodel
+
+def compute_1p_dot_1(q):
+    pin.computeAllTerms(rmodel, rdata, q, vq)
+    p1 = rdata.oMi[joint1_idx].rotation.transpose()*(cp1 - rdata.oMi[joint1_idx].translation)
+    v1 = rdata.v[joint1_idx].linear
+    rw1 = np.matmul(p1,rdata.v[joint1_idx].angular)
+    return v1 + rw1
+
+def compute_1p_dot_1_deriv(q):
+    pin.computeAllTerms(rmodel, rdata, q, vq)
+    p1 = rdata.oMi[joint1_idx].rotation.transpose()*(cp1 - rdata.oMi[joint1_idx].translation)
+    jvd = pin.getJointVelocityDerivatives(rmodel, rdata, joint1_idx, pin.LOCAL)
+    v_dot = jvd[0][:3]
+    w_dot = jvd[0][3:]
+    return v_dot + np.matmul(p1, w_dot)
+
+def numdiff(f, x, eps=1e-8):
+    """Estimate df/dx at x with finite diff of step eps
+
+    Parameters
+    ----------
+    f : function handle
+        Function evaluated for the finite differente of its gradient.
+    x : np.ndarray
+        Array at which the finite difference is calculated
+    eps : float, optional
+        Finite difference step, by default 1e-6
+
+    Returns
+    -------
+    jacobian : np.ndarray
+        Finite difference of the function f at x.
+    """
+    xc = np.copy(x)
+    f0 = np.copy(f(x))
+    res = []
+    for i in range(len(x)):
+        xc[i] += eps
+        res.append(copy.copy(f(xc) - f0) / eps)
+        xc[i] = x[i]
+    return np.array(res).T
+
+if __name__ =="__main__":
+    
+    rmodel, cmodel, vmodel = panda_loader()
+    rdata = rmodel.createData()
+    cdata = cmodel.createData()
+        # Generating the meshcat visualizer
+    MeshcatVis = MeshcatWrapper()
+    vis, meshcatVis = MeshcatVis.visualize(
+        TARGET_POSE,
+        robot_model=rmodel,
+        robot_collision_model=cmodel,
+        robot_visual_model=vmodel,
 )
-OBSTACLE_GEOM_OBJECT.meshColor = BLUE
+    # Config & velocity
+    q = np.array([ 0.439,   0.9274 , 0.3113 , 0.3734 ,-0.2116,  1.1214 , 0.024])
+    vq = np.array([1,1,1,1,1,1,1])
+    
+    vis.display(q)
+    
+    pin.computeAllTerms(rmodel, rdata, q, vq)
+    pin.updateGeometryPlacements(rmodel, rdata, cmodel, cdata, q)
+    
+    joint1_idx = cmodel.geometryObjects[cmodel.getGeometryId("panda2_link7_sc_2")].parentJoint
 
-IG_OBSTACLE = cmodel.addGeometryObject(OBSTACLE_GEOM_OBJECT)
+    geom_id1_idx = cmodel.getGeometryId("panda2_link7_sc_2")
+    geom_id2_idx = cmodel.getGeometryId("obstacle")
+    # Hppfcl distance
+    req = hppfcl.DistanceRequest()
+    res = hppfcl.DistanceResult()
+    
+    hppfcl.distance(
+        cmodel.geometryObjects[geom_id1_idx].geometry,
+        cdata.oMg[geom_id1_idx],
+        cmodel.geometryObjects[geom_id2_idx].geometry,
+        cdata.oMg[geom_id2_idx],
+        req, 
+        res
+    )
 
-### INITIAL CONFIG OF THE ROBOT
-INITIAL_CONFIG = pin.neutral(rmodel)
+    cp1 = res.getNearestPoint1()
 
-### ADDING THE COLLISION PAIR BETWEEN A LINK OF THE ROBOT & THE OBSTACLE
-cmodel.geometryObjects[cmodel.getGeometryId("panda2_link7_sc_2")].meshColor = YELLOW_FULL
-cmodel.addCollisionPair(
-    pin.CollisionPair(cmodel.getGeometryId("panda2_link7_sc_2"), IG_OBSTACLE)
-)
-cdata = cmodel.createData()
-print(f'shape 1 : {cmodel.getGeometryId("panda2_link6_sc_2")}')
-print(f'shape 2 : {IG_OBSTACLE}')
-
-# Generating the meshcat visualizer
-MeshcatVis = MeshcatWrapper()
-vis, meshcatVis = MeshcatVis.visualize(
-    TARGET_POSE,
-    robot_model=rmodel,
-    robot_collision_model=cmodel,
-    robot_visual_model=vmodel,
-)
-q = np.array([ 0.439,   0.9274 , 0.3113 , 0.3734 ,-0.2116,  1.1214 , 0.024])
-vis.display(q)
-
-# ### INITIAL X0
-# q0 = INITIAL_CONFIG
-# x0 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
-# req = hppfcl.DistanceRequest()
-# res = hppfcl.DistanceResult()
-# q_test = np.array([ 2.02829e-01, 3.74201e-01, 3.06873e-01,-6.99667e-01,-8.18421e-02, 1.29950e+00, 1.43251e-02])
-# pin.updateGeometryPlacements(rmodel, rdata, cmodel, cdata, q_test)
-# print(
-#     hppfcl.distance(
-#         cmodel.geometryObjects[cmodel.getGeometryId("panda2_link6_sc_2")].geometry,
-#         cdata.oMg[cmodel.getGeometryId("panda2_link6_sc_2")],
-#         cmodel.geometryObjects[IG_OBSTACLE].geometry,
-#         cdata.oMg[IG_OBSTACLE],
-#         req, 
-#         res
-#     )
-# )
-# q_test = np.array([2.02997e-01, 3.71892e-01, 3.07120e-01,-6.98672e-01,-8.17971e-02, 1.29954e+00, 1.43237e-02])
-# pin.updateGeometryPlacements(rmodel, rdata, cmodel, cdata, q_test)
-# print(
-#     hppfcl.distance(
-#         cmodel.geometryObjects[cmodel.getGeometryId("panda2_link6_sc_2")].geometry,
-#         cdata.oMg[cmodel.getGeometryId("panda2_link6_sc_2")],
-#         cmodel.geometryObjects[IG_OBSTACLE].geometry,
-#         cdata.oMg[IG_OBSTACLE],
-#         req, 
-#         res
-#     )
-# )
+    print(f"1p_dot_1: {compute_1p_dot_1(q)}")
+    print(f"numdiff: {numdiff(compute_1p_dot_1, q)}")
+    print(f"deriv 1p_dot_1_: {compute_1p_dot_1_deriv(q)}")
+    print(f"diff: {numdiff(compute_1p_dot_1, q) - compute_1p_dot_1_deriv(q)}")
